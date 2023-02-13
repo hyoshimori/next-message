bf8f6287f522de5756a3332bc5205ff9725f22fa
Hiroto Yoshimori <hiroto.y.129@gmail.com>
2023-02-13 06:21:15 +0100

    fetch method added

--- hooks/useMessages.ts
+++ hooks/useMessages.ts
@@ -2,10 +2,21 @@ import { useState } from 'react'
 import { useAxios } from './useAxios'
 import * as MessageType from '@/types/Message';
 
-// const useMessages = (channelId: string) => {
 const useMessages = () => {
   const { axios } = useAxios();
   const [messages, setMessages] = useState<MessageType.Message[]>([]);
+
+  const fetchmessage = async (channelId: string) => {
+    const response = await axios.get<MessageType.Message[]>("messages.json");
+    // remove null
+    const data = Object.values(response.data).filter((v) => v);
+    // See if the message id is same as the id from the props
+    const messagesForChennel = data.filter(m => {
+      return m.channelId === channelId
+    })
+    setMessages(messagesForChennel);
+  };
+
   const postMessage = async (m: MessageType.Message) => {
     try{
       const response = await axios.post<MessageType.Message>(
@@ -22,7 +33,7 @@ const useMessages = () => {
     })
   }
 
-  return { messages, postMessage}
+  return { fetchmessage, messages, postMessage}
 }
 
 export default useMessages
--- pages/channels/[channelId].tsx
+++ pages/channels/[channelId].tsx
@@ -1,36 +1,27 @@
 import MessageTimeline from '@/components/MessageTimeline';
 import styles from '@/pages/channels/channelPage.module.css'
 import SendMessageBox from "@/components/SendMessageBox";
-
+import useMessages from "@/hooks/useMessages";
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 const ChannelPage = () => {
-  const ms = [
-  {
-  id: "1",
-  body: "Hello channel",
-  channelId: "1",
-  createdAt: new Date(),
-  },
-  {
-  id: "2",
-  body: "Hello next",
-  channelId: "2",
-  createdAt: new Date(),
-  },
-  {
-  id: "1",
-  body: "Hello channel",
-  channelId: "1",
-  createdAt: new Date(),
-  },
-];
 
+  const { query } = useRouter();
+
+  const {messages, fetchmessage} = useMessages();
+
+  useEffect(() => {
+    fetchmessage(query.channelId as string);
+  }, [query.channelId as string])
+  // ↑useEffect depend on query.channelId. So add this to the rendering process
+  // ↑This allows rerendering when the channel is swiched
   return(
     <div className={styles.background}>
       <div className={styles.header}>
         <h2>Title</h2>
       </div>
-      <MessageTimeline messages={ms}></MessageTimeline>
+      <MessageTimeline messages={messages}></MessageTimeline>
       {/* <div>SendMessages</div> */}
       <SendMessageBox></SendMessageBox>
     </div>
